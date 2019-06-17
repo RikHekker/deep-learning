fa48c2102b815f167f1551119bb6a05083b6f591
fejhekker <31843274+fejhekker@users.noreply.github.com>
2019-06-17 17:34:09 +0200

    smaller file

--- deeplearningsegmentation.py
+++ deeplearningsegmentation.py
@@ -1,42 +1,14 @@
 import tensorflow as tf
-import tensorflow.keras.backend as K
-import math
-import tensorflow.keras as k
 from tensorflow.keras import layers
-from tensorflow.keras import callbacks
 from tensorflow.keras.applications.resnet50 import ResNet50
-#from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
-import sklearn.metrics
-import matplotlib.pyplot as plt
 import glob
-#from __future__ import print_function, absolute_import, division
-#from collections import namedtuple
 tf.enable_eager_execution()
-#sess = tf.Session()
-#sess.run(tf.local_variables_initializer())
 
 def gen(datasettype):
-   #takes the paths of the images and label images, encodes it and zip the appropriate files together  
-# =============================================================================
-#   if datasettype == 'train':
-#       path = 'D:/s141533/NeuralNetworks/TrainingSetCityScapes/leftImg8bit_trainvaltest/leftImg8bit/train/b*/*'
-#       path2 = "D:/s141533/NeuralNetworks/TrainingSetCityScapes/gtFine/train/b*/*labelIds*"  
-#   elif datasettype == 'validation':
-#       path = 'D:/s141533/NeuralNetworks/TrainingSetCityScapes/leftImg8bit/val/lindau/*'
-#       path2 = 'D:/s141533/NeuralNetworks/TrainingSetCityScapes/gtFine/val/lindau/*labelIds*'
-# =============================================================================
-  #path = 'D:/s141533/NeuralNetworks/TrainingSetCityScapes/leftImg8bit_trainvaltest/leftImg8bit/train/aachen/aachen_000000_000019_leftImg8bit*'
-  #path2 = "D:/s141533/NeuralNetworks/TrainingSetCityScapes/gtFine/train/aachen/aachen_000000_000019_gtFine_labelIds*"
-  
-  #path = 'D:/s141533/NeuralNetworks/TrainingSetCityScapes/Testimg/*'
-  #path2 = 'D:/s141533/NeuralNetworks/TrainingSetCityScapes/Testlabels/*'
-  #path='kaasbomber/deep-learning/TrainingSetCityScapes/leftImg8bit_trainvaltest/leftImg8bit/train/b*/*' 
-  #path2='kaasbomber/deep-learning/TrainingSetCityScapes/gtFine/train/b*/*labelIds*' 
-  
+ 
   path='/home/kaasbomber/deep-learning/TrainingSetCityScapes/leftImg8bit/train/b*/*' 
-  path2='/home/kaasbomber/deep-learning/TrainingSetCityScapes/gtFine/train/b*/*labelIds*' 
-  
+  path2='/home/kaasbomber/deep-learning/TrainingSetCityScapes/gtFine/train/b*/*labelIds*'
+      
   images_path=glob.glob(path)#glob.glob(path+"/leftImg8bit/train/aachen/*")
   images_path=[x.encode('utf-8') for x in images_path]  
   labels_path=glob.glob(path2)#glob.glob(path+"/gtFine_trainvaltest/gtFine/train/aachen/aachen_[]_gtFine_labellds.png")
@@ -70,192 +42,33 @@ def input_fn(batch_size_,datasettype):
   dataset = dataset.prefetch(None)
   return dataset
 
-def test_data(batch_size_):
-  #test if the data is what is expected
-  dataset=input_fn(batch_size_)
-  it=dataset.make_one_shot_iterator()
-  next_element=it.get_next()
-  for i in range(batch_size_):
-    image,label=next_element
-    image=image[i,:,:,:]*255
-    image=tf.cast(image, tf.int32)
-    label=label[i,:,:,:]
-    palette= [[0,0,0], [128,64,128],[244,35,232],[70,70,70],[102,102,156],[190,153,153],[153,153,153],[250,170,30],[220,220,  0],[107,142, 35],[152,251,152],[ 70,130,180],[220, 20, 60],[255,  0,  0],[  0,  0,142],[  0,  0, 70],[  0, 60,100],[  0, 80,100],[  0,  0,230],[119, 11, 32]]
-    label=tf.gather(palette, label)
-    label = tf.squeeze(label)
-  plt.imshow(image)
-  plt.show()
-  plt.imshow(label)
-  plt.show()
-
-def sparse_Mean_IOU(y_true, y_pred, class_id):
-    iou = []
-    pred_pixels = K.argmax(y_pred, axis=-1)
-    i=class_id
-    true_labels = K.equal(y_true[:,:,0], i)
-    pred_labels = K.equal(pred_pixels, i)
-    inter = tf.to_int32(true_labels & pred_labels)
-    union = tf.to_int32(true_labels | pred_labels)
-    legal_batches = K.sum(tf.to_int32(true_labels), axis=1)>0
-    ious = K.sum(inter, axis=1)/K.sum(union, axis=1)
-    iou.append(K.mean(tf.gather(ious, indices=tf.where(legal_batches)))) # returns average IoU of the same objects
-    iou = tf.stack(iou)
-    legal_labels = ~tf.debugging.is_nan(iou)
-    iou = tf.gather(iou, indices=tf.where(legal_labels))
-    return K.mean(iou)
-
-def fn0(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,0)	
-def fn1(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,1)	
-def fn2(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,2)	
-def fn3(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,3)	
-def fn4(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,4)	
-def fn5(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,5)	
-def fn6(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,6)	
-def fn7(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,7)	
-def fn8(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,8)	
-def fn9(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,9)	
-def fn10(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,10)	
-def fn11(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,11)
-def fn12(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,12)	
-def fn13(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,13)	
-def fn14(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,14)	
-def fn15(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,15)	
-def fn16(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,16)	
-def fn17(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,17)	
-def fn18(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,18)	
-def fn19(y_true, y_pred):return sparse_Mean_IOU(y_true,y_pred,19)	
-
-# =============================================================================
-# def Mean_IOU(y_true, y_pred):
-#     nb_classes = K.int_shape(y_pred)[-1]
-#     iou = []
-#     true_pixels = K.argmax(y_true, axis=-1)
-#     pred_pixels = K.argmax(y_pred, axis=-1)
-#     void_labels = K.equal(K.sum(y_true, axis=-1), 0)
-#     for i in range(0, nb_classes): # exclude first label (background) and last label (void)
-#         true_labels = K.equal(true_pixels, i) & ~void_labels
-#         pred_labels = K.equal(pred_pixels, i) & ~void_labels
-#         inter = tf.to_int32(true_labels & pred_labels)
-#         union = tf.to_int32(true_labels | pred_labels)
-#         legal_batches = K.sum(tf.to_int32(true_labels), axis=1)>0
-#         ious = K.sum(inter, axis=1)/K.sum(union, axis=1)
-#         iou.append(K.mean(tf.gather(ious, indices=tf.where(legal_batches)))) # returns average IoU of the same objects
-#     iou = tf.stack(iou)
-#     legal_labels = ~tf.debugging.is_nan(iou)
-#     iou = tf.gather(iou, indices=tf.where(legal_labels))
-#     return K.mean(iou)
-# =============================================================================
 
 def TheModel(batch_size_,restore):
   #build a model and get the loss and accuracy
-# =============================================================================
-#   tfe = tf.contrib.eager
-#   saver = tfe.Saver(model.variables)
-# =============================================================================
+
   dataset=input_fn(batch_size_,datasettype = 'train')
-  datasetval = input_fn(batch_size_,datasettype = 'validation')
-  metrics =["accuracy",fn0,fn1,fn2,fn3,fn4,fn5,fn6,fn7,fn8,fn9,fn10,fn11, fn12, fn13, fn14, fn15, fn16,fn17,fn18,fn19]
-  if restore == True:
-      model = tf.contrib.saved_model.load_keras_model('./saved_models/1560507394')
-  else:
-      model=tf.keras.models.Sequential()
-      inputs=tf.keras.layers.Input(shape=(256,512,3))
-      model.add(ResNet50(include_top=False, weights='imagenet',input_tensor=inputs, pooling=None, classes = 20))
-      #kernel_constraint = tf.keras.constraints.max_norm(1.),activation = 'relu',kernel_regularizer = k.regularizers.l2(l=0.1)
-      model.add(tf.keras.layers.Conv2D(20,(3,3)))
-      #model.add(tf.keras.layers.Dense(units = 20))
-      model.add(layers.BatchNormalization())   
-      #model.add(layers.Dropout(rate=0.5))
-      model.add(tf.keras.layers.Lambda(lambda x: tf.image.resize_bilinear(x,(256,512))))
-      #model.add(tf.keras.layers.Softmax(axis=-1))
-      model.summary()
-  #global_step = tf.Variable(0, trainable=False)
+  model=tf.keras.models.Sequential()
+  inputs=tf.keras.layers.Input(shape=(256,512,3))
+  model.add(ResNet50(include_top=False, weights='imagenet',input_tensor=inputs, pooling=None, classes = 20))
+  model.add(tf.keras.layers.Conv2D(20,(3,3)))
+  model.add(layers.BatchNormalization())   
+  #model.add(layers.Dropout(rate=0.5))
+  model.add(tf.keras.layers.Lambda(lambda x: tf.image.resize_bilinear(x,(256,512))))
+ 
   starter_learning_rate = 0.002
   #learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,
   #                                         58, 0.96, staircase=True)
   sgd = tf.keras.optimizers.SGD(starter_learning_rate, momentum=0.8, nesterov=True)
   model.compile(optimizer=sgd,   #SGD with momemtum usually gives good enough results without too many parameters as is the case for ADAMoptimizer
-                loss=tf.keras.losses.CategoricalCrossentropy(from_logits = True), #https://gombru.github.io/2018/05/23/cross_entropy_loss/ This site gives good info about how it works it is basically a sofmax function on the groundtruth scores for the classes followed by the cross-entropy loss on the result. The CE simply calculates a score for a ground truths of all classes compared with the log of s ground truth. CE = -log ( \frac{e^{s_{p}}}{\sum_{j}^{C} e^{s_{j}}} 
-                metrics=metrics) #accuracy = (tp + tn) / (tp + fp + fn + tn), precision = TP / (TP + FP), recall = TP / (TP + FN) with TP = tf.count_nonzero(predicted * actual) TN = tf.count_nonzero((predicted - 1) * (actual - 1)) FP = tf.count_nonzero(predicted * (actual - 1)) FN = tf.count_nonzero((predicted - 1) * actual), rmse : root of squared differences between scores and ground truths  
-  history=model.fit(dataset, epochs=2, steps_per_epoch=1) #174/6 for Aachen - 2975 Training Images - 1525 Test Images - 500 validation Images
+                loss=tf.keras.losses.CategoricalCrossentropy(from_logits = True) #https://gombru.github.io/2018/05/23/cross_entropy_loss/ This site gives good info about how it works it is basically a sofmax function on the groundtruth scores for the classes followed by the cross-entropy loss on the result. The CE simply calculates a score for a ground truths of all classes compared with the log of s ground truth. CE = -log ( \frac{e^{s_{p}}}{\sum_{j}^{C} e^{s_{j}}} 
+                ) #accuracy = (tp + tn) / (tp + fp + fn + tn), precision = TP / (TP + FP), recall = TP / (TP + FN) with TP = tf.count_nonzero(predicted * actual) TN = tf.count_nonzero((predicted - 1) * (actual - 1)) FP = tf.count_nonzero(predicted * (actual - 1)) FN = tf.count_nonzero((predicted - 1) * actual), rmse : root of squared differences between scores and ground truths  
+  model.fit(dataset, epochs=2, steps_per_epoch=1) #174/6 for Aachen - 2975 Training Images - 1525 Test Images - 500 validation Images
   #history=model.fit(dataset,validation_data=datasetval, epochs=2, steps_per_epoch=103,validation_steps=14) #174/6 for Aachen - 2975 Training Images - 1525 Test Images - 500 validation Images
   saved_model_path = tf.contrib.saved_model.save_keras_model(model, "./saved_models/")
-
-  #print(history.history.keys())
-  #print(saved_model_path)
-# =============================================================================
-#   for layer in model.layers:
-#     weights = layer.get_weights() # list of numpy arrays
-#     #print(len(weights),tf.reduce_mean(weights),tf.reduce_max(weights),tf.reduce_min(weights))
-#   print("model inputs")
-#   print(model.input,inputs)
-#   print("model outputs")
-#   print(model.output)
-# =============================================================================
-
-# =============================================================================
-#   plt.plot(history.history['acc'])
-#   plt.title('model accuracy')
-#   plt.ylabel('accuracy')    
-#   plt.xlabel('epoch')
-#   plt.show()
-# =============================================================================
-  
-# =============================================================================
-#   plt.plot(history.history['loss'])
-#   plt.plot(history.history['val_loss'])
-#   plt.title('model loss')
-#   plt.ylabel('loss')
-#   plt.xlabel('epoch')
-#   plt.legend(['train', 'validation'], loc='upper left')
-#   plt.show()
-#  
-#     
-#   plt.plot(history.history['sparse_Mean_IOU'])
-#   plt.plot(history.history['val_sparse_Mean_IOU'])
-#   plt.title('model Mean_IOU')
-#   plt.ylabel('Mean_IOU')
-#   plt.xlabel('epoch')
-#   plt.legend(['train', 'validation'], loc='upper left')
-#   plt.show()
-# =============================================================================
-  return model
-
-def test_model(batch_size_,restore):
-  #test if the output is what is expected; prints model generated images of the last batch
-  dataset=input_fn(batch_size_,datasettype = 'train')
-  #datasetval = input_fn(batch_size_,datasettype = 'validation')
-  model=TheModel(batch_size_,restore)
-  #output=model.predict(dataset,steps=1)
-  #print(output)
-  it=dataset.make_one_shot_iterator()
-  next_element=it.get_next()
-  for i in range(batch_size_):
-    image_original,label_original=next_element
-    output = model.predict_on_batch(image_original)
-    pred = output[i,:,:,:]
-    #print("output")
-    #print(pred)
-    pred=tf.keras.backend.argmax(pred,axis=-1)
-    #print("post-argmax predictions")
-    #print(pred) 
-    palette= [[0,0,0], [128,64,128],[244,35,232],[70,70,70],[102,102,156],[190,153,153],[153,153,153],[250,170,30],[220,220,  0],[107,142, 35],[152,251,152],[ 70,130,180],[220, 20, 60],[255,  0,  0],[  0,  0,142],[  0,  0, 70],[  0, 60,100],[  0, 80,100],[  0,  0,230],[119, 11, 32]]
-    pred=tf.gather(palette, pred)    
-    image_original = image_original[i,:,:,:]*255
-    #image_original=(image_original[i,:,:,:]+1)*127.5
-    image_original=tf.cast(image_original, tf.int32)
-    label_original=label_original[i,:,:,:]
-    label_original=tf.gather(palette, label_original)
-    label_original=tf.squeeze(label_original)
-    plt.imshow(pred)
-    plt.show()
-    plt.imshow(image_original)
-    plt.show()
-    plt.imshow(label_original)
-    plt.show()
+  print(saved_model_path)
  
+
 if __name__=='__main__':
   restore = False  
   batch_size_ = 4
-  #test_data(batch_size_)
-  test_model(batch_size_,restore)
+  TheModel(batch_size_,restore)
